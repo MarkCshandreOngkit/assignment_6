ba63256beb61fed48bf5a623061b7d738613f150
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-22 14:08:31 +0800

    convert to pascal and print

--- batch_5/print_full_name_pascal/print_full_name_pascal.py
+++ batch_5/print_full_name_pascal/print_full_name_pascal.py
@@ -1,4 +1,9 @@
 #input full name
 full_name = input("Input: ")
 #convert to pascal casing
+full_name = full_name.title()
+full_name = full_name.split()
+full_name = "".join(full_name)
 #print full name
+print(f"Output: {full_name}")
+
