828b2b56274f721a4e88f92529da6b1e1bf995e4
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-22 13:19:46 +0800

    input number(0-1000 only)

--- batch_5/print_six_digit_number/print_six_digit_number.py
+++ batch_5/print_six_digit_number/print_six_digit_number.py
@@ -1,3 +1,6 @@
 #input number(0-1000)
-#format number to six digit
-#print number
+number = int(input("Input(0-1000): "))
+if number <= 1000 and number >= 0:
+    print(number)
+    #format number to six digit
+    #print number
