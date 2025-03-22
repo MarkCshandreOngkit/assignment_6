32a1919b76ef44ae5b7f544196cd4633c082506a
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-22 13:26:52 +0800

    format and print output

--- batch_5/print_six_digit_number/print_six_digit_number.py
+++ batch_5/print_six_digit_number/print_six_digit_number.py
@@ -1,6 +1,7 @@
 #input number(0-1000)
 number = int(input("Input(0-1000): "))
 if number <= 1000 and number >= 0:
-    print(number)
     #format number to six digit
+    six_digit = "{:06}".format(number)
     #print number
+    print(f"Output: {six_digit}")
