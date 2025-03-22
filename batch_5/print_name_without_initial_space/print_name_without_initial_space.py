d3c4a7baf571eff87034bd3cc8da3ee0feaaf16f
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-22 12:18:41 +0800

    strip initial space and print output

--- batch_5/print_name_without_initial_space/print_name_without_initial_space.py
+++ batch_5/print_name_without_initial_space/print_name_without_initial_space.py
@@ -1,4 +1,6 @@
 #input full name
-full_name = input("Type Full Name: ")
+full_name = input("Input: ")
 #strip initial spaces
+full_name = full_name.lstrip()
 #print full name
+print(f"Output: {full_name}")
