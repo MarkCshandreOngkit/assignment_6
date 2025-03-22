79d09a07b4c68947be0120a9460edda41a63d3e1
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-22 13:53:51 +0800

    check number of words and print

--- batch_5/print_number_of_words/print_number_of_words.py
+++ batch_5/print_number_of_words/print_number_of_words.py
@@ -1,4 +1,14 @@
 #input statement
 statement = input("Input: ")
+
 #check number of words
+#split statement to words
+number = 0
+words = statement.split()
+#iterate through each word
+for word in words:
+    #add 1 per word
+    number += 1
 #print number of words
+print(number)
+
