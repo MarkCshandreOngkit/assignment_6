88eac9a1b248a477a43499883268cce4a5dadbd2
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-22 14:00:52 +0800

    count character and print

--- batch_5/print_number_of_characters/print_number_of_characters.py
+++ batch_5/print_number_of_characters/print_number_of_characters.py
@@ -1,4 +1,8 @@
 #input full name
 full_name = input("Input: ")
 #check number of characters
+number = 0
+for character in full_name:
+    number += 1
 #print number of characters
+print(f"Output: {number}")
