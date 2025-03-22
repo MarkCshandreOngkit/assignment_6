760f43b4d15114af295a6709d8fad68f2f246c1b
MarkCshandreOngkit <markongkit.001@gmail.com>
2025-03-22 14:16:41 +0800

    convert to snake case and print

--- batch_5/print_full_name_snake_case/print_full_name_snake_case.py
+++ batch_5/print_full_name_snake_case/print_full_name_snake_case.py
@@ -1,4 +1,8 @@
 #input full name
 full_name = input("Input: ")
 #convert to snake casing
+full_name = full_name.lower()
+full_name = full_name.split()
+full_name = "_".join(full_name)
 #print full name
+print(f"Output: {full_name}")
